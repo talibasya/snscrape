c1ae909ab1d41045761e6bde4fb8cbedc7627602
Marek294 <Markra5835@gmail.com>
2023-03-15 09:24:48 +0100

    handle empty response

--- snscrape/modules/twitter.py
+++ snscrape/modules/twitter.py
@@ -1481,6 +1481,8 @@ class TwitterUserScraper(TwitterSearchScraper):
 			endpoint = 'https://twitter.com/i/api/graphql/I5nvpI91ljifos1Y3Lltyg/UserByRestId'
 		variables = {fieldName: str(self._user), 'withSafetyModeUserFields': True, 'withSuperFollowsUserFields': True}
 		obj = self._get_api_data(endpoint, _TwitterAPIType.GRAPHQL, params = variables)
+    if not obj['data'] or 'result' not in obj['data']['user']:
+			return None
 		if not obj['data'] or obj['data']['user']['result']['__typename'] == 'UserUnavailable':
 			return None
 		user = obj['data']['user']['result']
